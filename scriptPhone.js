bb70a867f611169026e5ef0954d10de8f7fcb4d0
Stephanie <stepha@Stephanies-MacBook-Air.local>
2019-02-07 02:21:10 -0600

    okay now we are getting somewhere

--- scriptPhone.js
+++ scriptPhone.js
@@ -1,17 +1,26 @@
 var _2 = ['a','b','c'];
 var _3 = ['d','e','f'];
 
+function lister (){
+
+}
+
 var userEntry = prompt("What is your phone number?");
 
 // _2.length = 3
 
 // name is which digit we are looking at (ex: 2)
 // function will list all letters associated with that digit
-function letterList(name){
-	counter2 = 0
-	while (counter2 < name.length){
-		console.log(name[counter2]);
+function letterList(name1, name2){
+	counter1 = 0;
+	counter2 = 0;
+	while (counter2 < name2.length){
+		while (counter1 < name1.length){
+			console.log(name1[counter1]+name2[counter2]);
+			counter1++;
+		}
 		counter2++;
+		counter1 = 0;
 	}
 }
 
@@ -26,6 +35,24 @@ function createArray(name){
 		counter2++;
 	}
 }
+
+createArray(userEntry); // new array is newUserArray
+
+function numToLetters(name){
+	i = 0
+	while (i < name.length){
+		if (newUserArray[i] === "_2"){
+			// newUserArray[i] = ['a','b','c']
+			newUserArray[i] = [_2]
+		}
+		console.log(name[i]);
+		i++;
+	}
+}
+
+// }
+// if (newUserArray[i] === "_2"){
+//     newUserArray[i] = ['a','b','c']}
 
 // // name is which digit we are looking at (ex: 2)
 // // function will list all letters associated with that digit
