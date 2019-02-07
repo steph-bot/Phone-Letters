94608dae0f9cf688d2b2a9bcd6797550cd3e80a3
Stephanie <stepha@Stephanies-Air.attlocal.net>
2019-02-07 02:41:19 -0600

    algorithm progressing

--- scriptPhone.js
+++ scriptPhone.js
@@ -1,26 +1,31 @@
 var _2 = ['a','b','c'];
 var _3 = ['d','e','f'];
+var _4 = ['g','h','i'];
 
-function lister (){
 
-}
 
-var userEntry = prompt("What is your phone number?");
+// var userEntry = prompt("What is your phone number?");
 
 // _2.length = 3
 
 // name is which digit we are looking at (ex: 2)
 // function will list all letters associated with that digit
-function letterList(name1, name2){
+function letterList(name1, name2, name3){
 	counter1 = 0;
 	counter2 = 0;
-	while (counter2 < name2.length){
-		while (counter1 < name1.length){
-			console.log(name1[counter1]+name2[counter2]);
-			counter1++;
+	counter3 = 0;
+	while (counter3 < name3.length){
+		while (counter2 < name2.length){
+			while (counter1 < name1.length){
+				console.log(name1[counter1]+name2[counter2]+name3[counter3]);
+				counter1++;
+			}
+			counter2++;
+			counter1 = 0;
 		}
-		counter2++;
-		counter1 = 0;
+		counter3++;
+		counter2=0;
+		counter1=0;
 	}
 }
 
