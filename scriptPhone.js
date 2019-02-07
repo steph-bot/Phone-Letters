46bc5be4f9b5f97c763c14495e1ed305f44ccad0
Stephanie <stepha@Stephanies-MacBook-Air.local>
2019-02-07 01:13:18 -0600

    think ive got a plan for this algo

--- scriptPhone.js
+++ scriptPhone.js
@@ -4,16 +4,13 @@ var three = ['d','e','f'];
 two.length
 three.length
 
-// var counter = two.length - 1;
-
-// while (counter >= 0){
-// 	console.log(two[counter]);
-// 	counter--;
-// }
-
-counter2 = 0
-
-while (counter2 < two.length){
-		console.log(two[counter2]);
-	counter2++;
+// name is which digit we are looking at (ex: two)
+// function will list all letters associated with that digit
+function letterList(name){
+	counter2 = 0
+	while (counter2 < name.length){
+		console.log(name[counter2]);
+		counter2++;
+	}
 }
+
