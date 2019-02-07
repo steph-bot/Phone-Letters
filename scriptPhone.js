a58effd105b496fbae8ac80fc8ea58c196883ce8
Stephanie <stepha@Stephanies-MacBook-Air.local>
2019-02-07 01:07:02 -0600

    working on algorithm

--- scriptPhone.js
+++ scriptPhone.js
@@ -3,3 +3,17 @@ var three = ['d','e','f'];
 
 two.length
 three.length
+
+// var counter = two.length - 1;
+
+// while (counter >= 0){
+// 	console.log(two[counter]);
+// 	counter--;
+// }
+
+counter2 = 0
+
+while (counter2 < two.length){
+		console.log(two[counter2]);
+	counter2++;
+}
